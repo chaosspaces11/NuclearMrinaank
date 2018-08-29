b6bf9dde236c660d94efdd53c18e729868570012
Michael Chen <chaosspaces11@gmail.com>
2018-08-29 19:50:46 +1000

    Initial Commit
    Intial Commit

---
+++ cmake-build-debug/CMakeFiles/NuclearMrinaank.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NuclearMrinaank.dir/main.cpp.o"
+  "NuclearMrinaank.pdb"
+  "NuclearMrinaank"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NuclearMrinaank.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

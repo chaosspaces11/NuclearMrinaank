1a1d1c513d8330cbc5b9c7ce8fe1fe804f653d14
Michael Chen <chaosspaces11@gmail.com>
2018-08-30 20:42:09 +1000

    Implemented a debugging output for arrays.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,33 @@
 #include "Cell.cpp"
 #include "Grid.cpp"
 
+//
+void arrayDebugOutput(int arrayPtr[], int arrayLength)
+{
+    int dataLength = arrayLength/sizeof(arrayPtr[0]);
+    std::cout << "[";
+    for (int index = 0; index < dataLength; index++)
+    {
+        if ((index + 1) != dataLength)
+        {
+            std::cout << *(arrayPtr + index) << ",";
+        }
+        else if (index + 1 == dataLength)
+        {
+            std::cout << *(arrayPtr + index) << "]" << std::endl;
+        }
+        else
+        {
+            std::cout << "This message should never be printed" << std::endl;
+        }
+    }
+}
 
 int main()
 {
+    int testing[4] = {1,2,3,4};
+    arrayDebugOutput(testing, sizeof(testing));
+
     Grid test2;
     Cell test;
     return 0;
